ca6a99b5ef5101dc7ef863626e024c2698ecb714
RafaelPereiraa <rafaelsilvapereiraa@gmail.com>
2025-01-24 12:40:56 -0300

    Alterado parâmetro para consultar a API em homologação. Adicionado codigos ao enum de status da fatura.

--- src/main/java/io/t3w/correios/faturas/enums/T3WCorreiosFaturasStatus.java
+++ src/main/java/io/t3w/correios/faturas/enums/T3WCorreiosFaturasStatus.java
@@ -9,21 +9,27 @@ public enum T3WCorreiosFaturasStatus {
     // Fixme: Swagger dos correios está incompleto após alguma atualização, não mostrando mais os valores dos campos.
     //  Por hora, 'Paga', 'Estornada' e 'Em aberto' foram os valores que foi possível confirmar para o status, mas possivelmente existem outros.
     //  Adicionado desconhecido para não ficar vazio caso aconteça um novo tipo.
-    PAGA("Paga"),
-    ESTORNADA("Estornada"),
-    EM_ABERTO("Em aberto"),
     @JsonEnumDefaultValue
-    DESCONHECIDO("Desconhecido");
+    DESCONHECIDO(0,"Desconhecido"),
+    PAGA(1,"Paga"),
+    EM_ABERTO(2,"Em aberto"),
+    ESTORNADA(3,"Estornada");
 
+    private final int codigo;
     private final String descricao;
 
-    T3WCorreiosFaturasStatus(final String descricao) {
+    T3WCorreiosFaturasStatus(final int codigo, final String descricao) {
+        this.codigo = codigo;
         this.descricao = descricao;
     }
 
     @JsonValue
     public String getDescricao() {
         return descricao;
+    }
+
+    public int getCodigo() {
+        return codigo;
     }
 
     public static T3WCorreiosFaturasStatus valueOfDescricao(final String descricao) {
