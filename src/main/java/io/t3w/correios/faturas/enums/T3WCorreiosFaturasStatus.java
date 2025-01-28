385a4680dd95c9ac2d568264b8c68e8d04ed4f02
RafaelPereiraa <rafaelsilvapereiraa@gmail.com>
2025-01-28 17:51:39 -0300

    Find by código enum fatura status

--- src/main/java/io/t3w/correios/faturas/enums/T3WCorreiosFaturasStatus.java
+++ src/main/java/io/t3w/correios/faturas/enums/T3WCorreiosFaturasStatus.java
@@ -3,8 +3,6 @@ package io.t3w.correios.faturas.enums;
 import com.fasterxml.jackson.annotation.JsonEnumDefaultValue;
 import com.fasterxml.jackson.annotation.JsonValue;
 
-import java.util.stream.Stream;
-
 public enum T3WCorreiosFaturasStatus {
     // Fixme: Swagger dos correios está incompleto após alguma atualização, não mostrando mais os valores dos campos.
     //  Por hora, 'Paga', 'Estornada' e 'Em aberto' foram os valores que foi possível confirmar para o status, mas possivelmente existem outros.
@@ -33,6 +31,20 @@ public enum T3WCorreiosFaturasStatus {
     }
 
     public static T3WCorreiosFaturasStatus valueOfDescricao(final String descricao) {
-        return Stream.of(values()).filter(g -> g.getDescricao().equalsIgnoreCase(descricao)).findFirst().orElse(null);
+        for (final T3WCorreiosFaturasStatus status : T3WCorreiosFaturasStatus.values()) {
+            if (status.getDescricao().equalsIgnoreCase(descricao)) {
+                return status;
+            }
+        }
+        return null;
+    }
+
+    public static T3WCorreiosFaturasStatus valueOfCodigo(final int codigo) {
+        for (final T3WCorreiosFaturasStatus status : T3WCorreiosFaturasStatus.values()) {
+            if (status.getCodigo() == codigo) {
+                return status;
+            }
+        }
+        return null;
     }
 }
