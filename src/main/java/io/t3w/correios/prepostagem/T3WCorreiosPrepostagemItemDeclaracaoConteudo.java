7131d820905be2059a01f78a3b7749e1c223a06a
Thiago Magno <thgmgn@gmail.com>
2025-07-17 15:15:40 -0300

    refactor(pre-postagem): Inclui itens obrigatórios da declaração de conteúdo e ajusta tipos
    - Adiciona o tratamento obrigatório `itensDeclaracaoConteudo` nos testes de pré-postagem para alinhamento com as alterações da API dos Correios
    - Atualiza `T3WCorreiosPrepostagemItemDeclaracaoConteudo` para usar os tipos corretos (`int` para quantidade, `BigDecimal` para valor)

--- src/main/java/io/t3w/correios/prepostagem/T3WCorreiosPrepostagemItemDeclaracaoConteudo.java
+++ src/main/java/io/t3w/correios/prepostagem/T3WCorreiosPrepostagemItemDeclaracaoConteudo.java
@@ -3,6 +3,8 @@ package io.t3w.correios.prepostagem;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.math.BigDecimal;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
 
 public class T3WCorreiosPrepostagemItemDeclaracaoConteudo {
@@ -10,15 +12,15 @@ public class T3WCorreiosPrepostagemItemDeclaracaoConteudo {
     private String conteudo;
 
     @JsonProperty("quantidade")
-    private String quantidade;
+    private int quantidade;
 
     @JsonProperty("valor")
-    private String valor;
+    private BigDecimal valor;
 
     public T3WCorreiosPrepostagemItemDeclaracaoConteudo() {
     }
 
-    public T3WCorreiosPrepostagemItemDeclaracaoConteudo(String conteudo, String quantidade, String valor) {
+    public T3WCorreiosPrepostagemItemDeclaracaoConteudo(String conteudo, int quantidade, BigDecimal valor) {
         this.conteudo = conteudo;
         this.quantidade = quantidade;
         this.valor = valor;
@@ -33,20 +35,20 @@ public class T3WCorreiosPrepostagemItemDeclaracaoConteudo {
         return this;
     }
 
-    public String getQuantidade() {
+    public int getQuantidade() {
         return quantidade;
     }
 
-    public T3WCorreiosPrepostagemItemDeclaracaoConteudo setQuantidade(String quantidade) {
+    public T3WCorreiosPrepostagemItemDeclaracaoConteudo setQuantidade(int quantidade) {
         this.quantidade = quantidade;
         return this;
     }
 
-    public String getValor() {
+    public BigDecimal getValor() {
         return valor;
     }
 
-    public T3WCorreiosPrepostagemItemDeclaracaoConteudo setValor(String valor) {
+    public T3WCorreiosPrepostagemItemDeclaracaoConteudo setValor(BigDecimal valor) {
         this.valor = valor;
         return this;
     }
