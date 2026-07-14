2a3cf420cb4cba70bcea592b056ea6ad3b217be0
rpereira <rafaelsilvapereiraa@gmail.com>
2026-07-14 18:36:39 -0300

    Normaliza valor do item para declaração de conteúdo Adiciona tratamento para retorno de erro somente com um campo 'msg' único, diferente do padrão da documentação.

--- src/main/java/io/t3w/correios/responses/T3WCorreiosResponseDefault.java
+++ src/main/java/io/t3w/correios/responses/T3WCorreiosResponseDefault.java
@@ -2,6 +2,7 @@ package io.t3w.correios.responses;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSetter;
 
 import java.time.LocalDateTime;
 
@@ -44,5 +45,27 @@ public class T3WCorreiosResponseDefault extends Throwable {
 
     public String[] getMsgs() {
         return msgs;
+    }
+
+    @JsonSetter("msgs")
+    public T3WCorreiosResponseDefault setMsgs(String[] msgs) {
+        this.msgs = msgs;
+        return this;
+    }
+
+    // Coisas da api dos correios... Acusa que erros 400 deveriam vir no padrão do objeto, mas em alguns casos retorna body com { "msg": "texto" }
+    @JsonSetter("msg")
+    public T3WCorreiosResponseDefault setMsg(String msg) {
+        if (msg != null) {
+            if (this.msgs == null) {
+                this.msgs = new String[]{msg};
+            } else {
+                String[] newMsgs = new String[this.msgs.length + 1];
+                System.arraycopy(this.msgs, 0, newMsgs, 0, this.msgs.length);
+                newMsgs[this.msgs.length] = msg;
+                this.msgs = newMsgs;
+            }
+        }
+        return this;
     }
 }
